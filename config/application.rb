a932e5ee5749d8b81ebdf8a9d8765ac140f03b85
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-01-30 16:53:19 +0800

    Configure and create helpers - Change include_all_helpers config to false - Split the _collapsible_elements.html.erb file's content into   partials and extract logic from the file into partials

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module BimasKatolik
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Disable loads all helpers
+    config.action_controller.include_all_helpers = false
   end
 end
