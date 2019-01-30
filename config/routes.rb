5a157db2a0a2622a282195beb5cf19effbdbe0ab
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-01-30 15:15:16 +0800

    Change login route to/masuk_ke_website_bimas_katolik

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :arsip
 
+  devise_scope :user do
+    get 'masuk_ke_website_bimas_katolik_sumba_tengah', to: 'devise/sessions#new'
+  end
+
   root to: 'web#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
